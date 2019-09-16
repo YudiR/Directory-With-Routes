030b7326c3c01d41f73eefd2d96ed2bbc8dcbc7b
YudiR <yudrose13@gmail.com>
2019-09-16 18:46:02 -0400

    added redirect on submit

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import {
-  Form,
-  Button
-} from "react-bootstrap";
-
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import {  withRouter } from 'react-router-dom'
 function Forms(props) {
   console.log("PROPS::", props);
-  let username = '';
-  let email = '';
-  let phone = '';
-  let website = '';
+  let username = "";
+  let email = "";
+  let phone = "";
+  let website = "";
 
-
-
+let routeChange = () =>{
+  props.history.push('/')
+}  
   return (
     <React.Fragment>
       <Form>
@@ -22,59 +20,64 @@ function Forms(props) {
           <Form.Control
             placeholder={props.username}
             onChange={event => {
-              username = event.target.value
-              console.log(username)
+              username = event.target.value;
+              console.log(username);
             }}
           />
           <Form.Label>Email</Form.Label>
           <Form.Control
             placeholder={props.email}
             onChange={event => {
-              email = event.target.value
-              console.log(email)
+              email = event.target.value;
+              console.log(email);
             }}
           />
-           <Form.Label>Phone</Form.Label>
+          <Form.Label>Phone</Form.Label>
           <Form.Control
             placeholder={props.phone}
             onChange={event => {
-              phone = event.target.value
-              console.log(phone)
+              phone = event.target.value;
+              console.log(phone);
             }}
           />
-           <Form.Label>Website</Form.Label>
+          <Form.Label>Website</Form.Label>
           <Form.Control
             placeholder={props.website}
             onChange={event => {
-              website = event.target.value
-              console.log(website)
+              website = event.target.value;
+              console.log(website);
             }}
           />
         </Form.Group>
-        
-        
+
         <Button
           variant="primary"
           type="submit"
           onClick={e => {
             e.preventDefault();
-            console.log("DATA::",{ id: props.id, un: username });
-            
-            props.submit({ id: props.id, un: username,email: email, website: website, phone: phone });
-          }}
+            console.log("DATA::", { id: props.id, un: username });
+
+            props.submit({
+              id: props.id,
+              un: username,
+              email: email,
+              website: website,
+              phone: phone
+            });
+         console.log(props.history)
+routeChange()  
+        }}
         >
           Submit
         </Button>
       </Form>
       <ul>
-            <Link to ="/">
-            <li>
-                Directory
-            </li>
-            </Link>
-            </ul>
+        <Link to="/">
+          <li>Directory</li>
+        </Link>
+      </ul>
     </React.Fragment>
   );
 }
 
-export default Forms;
+export default withRouter (Forms);
